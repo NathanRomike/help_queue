8c57d1575d616a9c48c8714471d791e7d54f08aa
Illia Kuchko and Nathan Romike <student@epicodus.com>
2016-02-29 11:03:02 -0800

    Add color-location helper to teacher homepage.

---
+++ tests/unit/helpers/color-location-test.js
@@ -0,0 +1,10 @@
+import { colorLocation } from 'help-queue/helpers/color-location';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | color location');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = colorLocation([42]);
+  assert.ok(result);
+});
