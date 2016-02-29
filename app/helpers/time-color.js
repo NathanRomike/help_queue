dbc2f00aba4f9328bc0d059809be7b3870fa213e
Illia Kuchko and Nathan Romike <ikuchko@gmail.com>
2016-02-29 13:19:49 -0800

    create time-color helper to change ticket color based on time

--- app/helpers/time-color.js
+++ app/helpers/time-color.js
@@ -4,15 +4,15 @@ export function timeColor(params/*, hash*/) {
   var time = params[0];
   var duration = moment(time).fromNow();
   var colors = {green: 'green', yellow: 'yellow', red: 'red'};
-  // if (duration.search("hour") > 0) {
-  //   return colors.red;
-  // } else if (moment(duration, 'm').format('m') < 15){
-  //   return colors.green;
-  // }
-  console.log(moment('11:58', "hh:mm").fromNow())
-  console.log(moment(moment('11:58', "hh:mm").fromNow(), 'm').format('m'))
-  var time = {}
-  return params;
+  if (duration.search("hour") > 0) {
+    return colors.red;
+  } else if (moment(duration, 'm').format('m') < 15) {
+    return colors.green;
+  } else if (moment(duration, 'm').format('m') > 15) {
+    return colors.yellow;
+  } else {
+    return colors.green;
+  }
 }
 
 export default Ember.Helper.helper(timeColor);
--- app/teacher/route.js
+++ app/teacher/route.js
@@ -4,4 +4,16 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findAll('ticket');
   },
+
+  currentTimeMetronome: function() {
+    var interval = 1000;
+    Ember.run.later(this, function() {
+      this.notifyPropertyChange('currentTimePulse');
+      this.currentTimeMetronome();
+    }, interval);
+  }.on('init'),
+
+  createdTime: function () {
+    return this.get('timestamp');
+  }.property('currentTimePulse'),
 });
